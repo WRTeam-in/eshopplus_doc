20ffc1d4d507ddb50272af98af60b51cc269ba8b
wrteam-priyansh <wrteam.priyansh@gmail.com>
2025-05-13 14:50:49 +0530

    updtaed the docuras.confg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,36 +6,25 @@
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
-
-
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  
   title: 'eShop Plus Documentation',
   tagline: 'Documentation for eShop Plus Panel & App',
   favicon: 'img/favicon.webp',
 
-  // Set the production url of your site here
-    url: "https://wrteam-in.github.io", // GitHub Pages domain
+  url: "https://wrteam-in.github.io", // GitHub Pages domain
   baseUrl: "/eshopplus_doc/", 
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'WRTeam-in', // Usually your GitHub org/user name.
-  projectName: 'eshopplus_doc', // Usually your repo name.
+  organizationName: 'WRTeam-in', // GitHub org/user name
+  projectName: 'eshopplus_doc', // GitHub repo name
+  deploymentBranch: "gh-pages", 
+  trailingSlash: true, 
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  trailingSlash: true, 
-  deploymentBranch: "gh-pages", 
 
- 
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -47,7 +36,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/docs', // change this from '/' to '/docs'
+          routeBasePath: '/', // <-- ✅ Change applied here
           sidebarPath: './sidebars.js',
         },
         blog: {
@@ -67,10 +56,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      deployment: {
-        useSSH: true,
-      },
-      // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'eShop Plus',
@@ -85,7 +70,6 @@ const config = {
             position: 'left',
             label: 'Documentation',
           },
-        
           {
             href: 'https://wrteam.in/',
             label: 'WRTeam',
@@ -95,7 +79,6 @@ const config = {
       },
       footer: {
         style: 'dark',
-        
         copyright: `Copyright © ${new Date().getFullYear()} WRTeam. All rights reserved.`,
       },
       prism: {
@@ -103,7 +86,6 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
-    
 };
 
 export default config;
